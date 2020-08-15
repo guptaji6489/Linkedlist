1a7b83697a97d16ed7eff94d8d6e13dce12de5bf
guptaji6489 <guptaji6489@gmail.com>
2020-08-15 21:58:38 +0530

    add one to linkedlist

---
+++ add_one_to_linkedlist.cpp
@@ -0,0 +1,77 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node* next;
+
+    node(int d)
+    {
+        data = d;
+        next=NULL;
+
+    }
+};
+void insertathead(node*&head,int data)
+{
+    node* n = new node(data);
+    n->next=head;
+    head=n;
+}
+int  length(node*head)
+{
+    int len = 0;
+    while(head!=NULL)
+    {
+        head = head->next;
+        len +=1;
+    }
+    return len;
+}
+
+void insertattail(node*&head,int data)
+{
+    if(head==NULL)
+    {
+        head = new node(data);
+        return;
+    }
+    node*tail = head;
+    while(tail->next!=NULL)
+    {
+        tail = tail->next;
+
+    }
+    tail->next = new node(data);
+    return;
+}
+
+
+void print(node*head)
+{
+    while(head->next!=NULL)
+    {
+        cout<<head->data;
+        head = head->next;
+    }
+    cout<<head->data +1;
+    cout<<endl;
+}
+
+
+int main()
+{
+    node*head = NULL;
+    insertattail(head,1);
+    insertattail(head,2);
+    insertattail(head,3);
+    //insertattail(head,20);
+
+    print(head);
+
+
+}
+
